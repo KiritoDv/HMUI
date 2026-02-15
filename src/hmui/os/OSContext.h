902a8b74920e15450f80c0d52a6ccb5bc513c3f0
MegaMech <MegaMech@users.noreply.github.com>
2026-02-14 21:11:47 -0700

    Update OSContext.h

--- src/hmui/os/OSContext.h
+++ src/hmui/os/OSContext.h
@@ -69,5 +69,6 @@ public:
     virtual void showCursor(bool show) = 0;
     virtual bool isGamepadAvailable(int id) = 0;
     virtual bool isGamepadButtonPressed(int id, ControllerButton button) = 0;
+    virtual bool IsKeyboardButtonPressed(int virtualKey) = 0;
     virtual float getGamepadAxis(int id, ControllerAxis axis) = 0;
 };
